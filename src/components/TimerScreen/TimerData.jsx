8387617e586b36e1719712586cbf65a0c491dfc5
AnasbinKabeer <hafizapk007@gmail.com>
2024-12-19 15:47:57 +0530

    timer data changed

--- src/components/TimerScreen/TimerData.jsx
+++ src/components/TimerScreen/TimerData.jsx
@@ -7,17 +7,17 @@ export const TimerData = [
     {
         id: "1",
         warning: "4:30",
-        final: "4:56"
+        final: "4:50"
     },
     {
         id: "2",
-        warning: "10:00",
-        final: "8:00"
+        warning: "8:00",
+        final: "9:50"
     },
     {
         id: "3",
-        warning: "12:00",
-        final: "15:00"
+        warning: "10:00",
+        final: "14:50"
     },    {
         id: "4",
         warning: "00:05",
